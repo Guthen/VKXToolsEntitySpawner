5f4d9e53a238e579147aab49ac334f5b1c1d4fa5
Guthen <33220603+Guthen@users.noreply.github.com>
2021-10-20 09:28:36 +0200

    Version 2.2.3.

--- lua/autorun/vkx_entspawner.lua
+++ lua/autorun/vkx_entspawner.lua
@@ -1,5 +1,5 @@
 vkx_entspawner = vkx_entspawner or {}
-vkx_entspawner.version = "2.2.2"
+vkx_entspawner.version = "2.2.3"
 vkx_entspawner.save_path = "vkx_tools/entspawners/%s.json"
 vkx_entspawner.spawners = vkx_entspawner.spawners or {}
 vkx_entspawner.blocking_entity_blacklist = {
